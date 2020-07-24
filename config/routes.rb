3d25e4485f27b48703744cf1cc943e5dbb07236d
Tyler Hall <tylerhall12@outlook.com>
2020-07-24 15:05:06 +1000

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :profiles
+
+  get "/", to: "profiles#home", as: "root"
+  get "/404", to: "profiles#not_found"
+  
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
