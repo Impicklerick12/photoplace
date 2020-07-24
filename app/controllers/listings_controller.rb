c59f5d3583d1b562083569e2636112ae87c61103
Tyler Hall <tylerhall12@outlook.com>
2020-07-24 15:49:03 +1000

    Altered listings to show on profile page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -32,7 +32,7 @@ class ListingsController < ApplicationController
 
     respond_to do |format|
       if @listing.save
-        format.html { redirect_to @listing, notice: 'Listing was successfully created.' }
+        format.html { redirect_to profile_path(@listing.profile_id), notice: 'Listing was successfully created.' }
         format.json { render :show, status: :created, location: @listing }
       else
         format.html { render :new }
