831b0ea2889dc7b6da704679124c3e595b92e7ee
Tyler Hall <tylerhall12@outlook.com>
2020-07-28 11:52:45 +1000

    Added helper for profile image assignment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+    def profile_photo(profile)
+        if profile.photo.attached?
+            profile.photo
+        else
+            image_url("user.png")
+        end
+    end
 end
