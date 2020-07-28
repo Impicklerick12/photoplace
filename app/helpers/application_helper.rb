da80253831bd627e80b2137a059f219550339db0
Tyler Hall <tylerhall12@outlook.com>
2020-07-28 16:04:18 +1000

    Added avatars to comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,13 @@ module ApplicationHelper
             image_url("user.png")
         end
     end
+
+    def review_photo(review)
+        if Profile.exists?(user_id: review)
+            review.profile.photo
+        else
+            image_url("user.png")
+        end
+    end
+
 end
