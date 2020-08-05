fa534f876e429698153aae42937fed8e5c7404d1
Tyler Hall <tylerhall12@outlook.com>
2020-08-05 10:59:10 +1000

    fixed error

--- db/migrate/20200727051605_create_reviews.rb
+++ db/migrate/20200727051605_create_reviews.rb
@@ -2,7 +2,6 @@ class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
       t.text :review
       t.timestamps
     end
